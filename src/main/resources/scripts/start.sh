9edac873358cee236b5c1ace34d7970809bf2e60
Oleksandr Tyshkovets <sg.olexander@gmail.com>
2016-12-26 12:17:58 +0200

    Fix start script and cleanup db init scripts

--- src/main/resources/scripts/start.sh
+++ src/main/resources/scripts/start.sh
@@ -5,12 +5,9 @@ then
   ENV="prod";
 fi
 
-#java -Dspring.profiles.active=${ENV} -Dspring.config.location=config/ -jar lib/matrix.server-*.jar
-
 nohup java -Xms2G -Xmx3G \
            -Dspring.profiles.active=${ENV} -Dspring.config.location=config/ \
            -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=logs \
            -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1098 \
            -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false \
            -jar lib/matrix.server-*.jar > /dev/null &
-/
