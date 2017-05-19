23dc44d4bb6fe05a1fc53ad5e63da7983ad06091
Oleksandr Tyshkovets <sg.olexander@gmail.com>
2017-05-19 18:00:50 +0300

    build(JMX): Configure JMX remote port/hostname

--- src/main/resources/scripts/start.sh
+++ src/main/resources/scripts/start.sh
@@ -10,5 +10,6 @@ nohup java -Xms2G -Xmx3G \
            -Dspring.profiles.active=${ENV} -Dspring.config.location=config/ \
            -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=logs \
            -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1098 \
+           -Dcom.sun.management.jmxremote.rmi.port=1098 -Djava.rmi.server.hostname=127.0.0.1 \
            -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false \
-           -jar lib/matrix.server-*.jar > /dev/null &
+           -jar lib/matrix.server-*.jar > output.out 2> error.err < /dev/null &
